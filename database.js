e0474880087af4280e68b06f96024509f9f8a855
Thanawat060 <6206021620060@fitm.kmutnb.ac.th>
2021-04-29 01:10:17 +0700

    update

--- database.js
+++ database.js
@@ -1,3 +1,3 @@
 module.exports={
-    db:'mongodb+srv://admin:X5nkY9phCBlkbkrd@cluster0.2e6fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    db:'mongodb+srv://admin:12345@cluster0.2e6fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
